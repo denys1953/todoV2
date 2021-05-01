6e8c61a97db81d83b8a433878eb9a2dd31a10f40
denys1953 <den25640@gmail.com>
2021-05-01 21:55:24 +0300

    master

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function buttonAdd() {
       out += `
          <div class="active">
             <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            <div>${input.value}</div>
+            <div class="checked">${input.value}</div>
             <button type="button" class="btn btn-danger">&#215;</button>
          </div>
       `
@@ -38,11 +38,26 @@ function buttonAdd() {
 function check() {
    let btn = document.getElementsByClassName("btn-danger")
    let checkBox = document.getElementsByClassName("form-check-input")
+   let active = document.getElementsByClassName("checked")
    for (let i = 0; i < btn.length; i++) {
       btn[i].addEventListener('click', clearButton)
    }
    for (let i = 0; i < checkBox.length; i++) {
       checkBox[i].addEventListener("click", checkBoxChecked)
+   }
+   for (let i = 0; i < active.length; i++) {
+      let m = 0
+      active[i].addEventListener('click', () => {
+         if (m == 0) {
+            active[i].parentNode.children[0].setAttribute('checked', '')
+            active[i].classList.add('check')
+            m++
+         } else {
+            active[i].parentNode.children[0].removeAttribute('checked')
+            active[i].classList.remove('check')
+            m--
+         }
+      })
    }
    localStorage.setItem('out', JSON.stringify(out2))
 }
@@ -56,10 +71,10 @@ function clearButton() {
 function checkBoxChecked() {
    if (this.checked) {
       this.parentNode.children[1].classList.add('check')
-      this.setAttribute('checked', '');
+      this.setAttribute('checked', '')
    } else {
       this.parentNode.children[1].classList.remove('check')
-      this.removeAttribute('checked');
+      this.removeAttribute('checked')
    }
    out2 = document.getElementById('todo-list').innerHTML
    localStorage.removeItem('out')
