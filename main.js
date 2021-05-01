2461911e765b6148d27e5b1f3a956ec7227f29f9
denys1953 <den25640@gmail.com>
2021-05-01 18:43:44 +0300

    master

---
+++ main.js
@@ -0,0 +1,67 @@
+let input = document.getElementById('input')
+let button = document.getElementById('out')
+let todo
+
+if (JSON.parse(localStorage.getItem('out')) == null) {
+   localStorage.setItem('out', JSON.stringify(''))
+}
+
+let out2 = JSON.parse(localStorage.getItem('out'))
+document.getElementById('todo-list').innerHTML = out2
+button.addEventListener('click', buttonAdd)
+document.addEventListener('keydown', (e) => {
+   if (e.keyCode == 13) {
+      buttonAdd()
+   }
+})
+check()
+
+function buttonAdd() {
+   todo = document.getElementById('todo-list')
+   if (input.value !== '') {
+      let out = ''
+      out += `
+         <div class="active">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <div>${input.value}</div>
+            <button type="button" class="btn btn-danger">&#215;</button>
+         </div>
+      `
+      todo.innerHTML += out
+      out2+=out
+      input.value = ''
+      input.focus()
+      check()
+   }
+}
+
+function check() {
+   let btn = document.getElementsByClassName("btn-danger")
+   let checkBox = document.getElementsByClassName("form-check-input")
+   for (let i = 0; i < btn.length; i++) {
+      btn[i].addEventListener('click', clearButton)
+   }
+   for (let i = 0; i < checkBox.length; i++) {
+      checkBox[i].addEventListener("click", checkBoxChecked)
+   }
+   localStorage.setItem('out', JSON.stringify(out2))
+}
+
+function clearButton() {
+   this.parentElement.remove()
+   out2 = document.getElementById('todo-list').innerHTML
+   localStorage.removeItem('out')
+   localStorage.setItem('out', JSON.stringify(out2))
+}
+function checkBoxChecked() {
+   if (this.checked) {
+      this.parentNode.children[1].classList.add('check')
+      this.setAttribute('checked', '');
+   } else {
+      this.parentNode.children[1].classList.remove('check')
+      this.removeAttribute('checked');
+   }
+   out2 = document.getElementById('todo-list').innerHTML
+   localStorage.removeItem('out')
+   localStorage.setItem('out', JSON.stringify(out2))
+}
